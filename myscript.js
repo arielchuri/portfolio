8e69a9025f0320818d602baf31a28cdeef0cbaa0
Ariel Churi <github@sparklelabs.com>
2024-01-04 23:16:50 -0500

    update

--- myscript.js
+++ myscript.js
@@ -1,10 +1,4 @@
 console.log("Hello world!");
-document.getElementById("footer").innerHTML =
-  '<hr class="zig"> <hr class="zag"> <div class="container"><div class="twelve columns small">' +
-  // '<h6>Colophon</h6>' +
-'<p style="padding:12px;">This document was written in vanilla HTML, javascript, and CSS. ' +
-  'The CSS started from <a href="http:www.getskeleton.com" target="_blank">Skeleton</a>. The typeface is <a href="https:rsms.me/inter/" target="_blank">Inter</a>. ' +
-  '🄯 Created by Ariel Churi in 2022. </p></div></section>' ;
 
 document.getElementById("returnbutton").innerHTML =
   '<a href="index.html" class="button button-primary">&#8617; Return</a>';
@@ -18,3 +12,66 @@ site = '&#115;&#112;&#97;&#114;&#107;&#108;&#101;&#108;&#97;&#98;&#115;&#46;&#99
 document.getElementById("emailbutton").innerHTML =
   '<a class="button button-primary" href=\"mailto:' + user + '@' + site + '\">' +
   user + '@' + site + '</a>' ;
+
+document.getElementById("footer").innerHTML = '<hr class="zig"> <hr class="zag"> <div class="container"><div class="twelve columns small"> \
+      <section class="" style="padding-bottom:0vh;padding-top:0vh"> \
+        <div class="row"> \
+          <div class="three columns"> \
+            <table> \
+              <thead> \
+                <tr> \
+                  <th></th> \
+                </tr> \
+              </thead> \
+              <tbody> \
+                <tr> \
+                  <td><strong>Ariel Churi</strong></td> \
+                </tr> \
+                <tr> \
+                  <td><a href="mailto:ac@sparklelabs.com">ac@sparklelabs.com</a></td> \
+                </tr> \
+                <tr> \
+                  <td><a href="tel:646-450-4576">(646) 450-4576</a></td> \
+                </tr> \
+                <tr> \
+                  <td><a href="https://arielchuri.sparklelabs.com">arielchuri.sparklelabs.com</a></td> \
+                </tr> \
+                <tr> \
+                  <td><a href="https://linkedin.com/in/arielchuri/">linkedin.com/in/arielchuri</a></td> \
+                </tr> \
+              </tbody> \
+            </table> \
+          </div> \
+          <div class="three columns bottomspace"> \
+            <table> \
+              <thead> \
+                <tr> \
+                  <th></th> \
+                </tr> \
+              </thead> \
+              <tbody> \
+                <tr> \
+                  <td><a href="https://goo.gl/maps/oHnA1aaUuyH7s5W99">Montclair, NJ</a></td> \
+                </tr> \
+                <tr> \
+                  <td>he/him</td> \
+                </tr> \
+                <tr> \
+                  <td><a href="https://arielchuri.sparklelabs.com/resume/pronounce.mp3">ari-EL choo-REE</a> <a href="https://arielchuri.sparklelabs.com/resume/pronounce.mp3"><img style="height:18px;width:18px;vertical-align:middle" src="resume/audio-document.svg"/></a></td> \
+                </tr> \
+                <tr> \
+                  <td><a href="https://arielchuri.sparklelabs.com/resume/arielchuri_resume.pdf">arielchuri_resume.pdf</a> <a href="https://arielchuri.sparklelabs.com/resume/arielchuri_resume.pdf"><img style="height:18px;width:18px;vertical-align:middle" src="resume/pdf-file.svg"></a></td> \
+                </tr> \
+                <tr> \
+                  <td><a href="https://github.com/arielchuri/">github.com/arielchuri</a></td> \
+                </tr> \
+              </tbody> \
+            </table> \
+          </div> \
+        </div> \
+      </section> ' +
+
+  // '<h6>Colophon</h6>' +
+'<p class="small" style="padding:0px;">This document was written in vanilla HTML, javascript, and CSS. ' +
+  'The CSS started from <a href="http:www.getskeleton.com" target="_blank">Skeleton</a>. The typeface is <a href="https:rsms.me/inter/" target="_blank">Inter</a>. ' +
+  '🄯 Created by Ariel Churi in 2022. </p></div></section>' ;
